dd1ef8611a106a4ec7dd6a96b7d94a68e3ed3011
ilya <ilyakhlamov77@gmail.com>
2023-05-09 18:55:07 +0300

    changed UserView.java

--- view/UserView.java
+++ view/UserView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserView {
     
     public int showUserMenu(){
-        System.out.println("1 - Подать заявку на фильм");
+        System.out.println("1 - Арендовать фильм");
         System.out.println("2 - Вернуть фильм");
 
         Scanner sc = new Scanner(System.in);
