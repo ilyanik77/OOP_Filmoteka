10ade0232011e0a1af4113bd4ffe6c2dd990ec6b
ilya <ilyakhlamov77@gmail.com>
2023-05-09 18:29:49 +0300

    add ListMovies.java

---
+++ model/ListMovies.java
@@ -0,0 +1,20 @@
+package model;
+
+//import java.io.*;
+import java.util.ArrayList;
+
+public class ListMovies {
+    
+    private ArrayList<Movie> listMovies;
+    
+    public ArrayList<Movie> getListMovies(){
+        return listMovies;
+    }
+
+    //public void writeMovieInFile(Movie movie, String fileName ){
+    //    FileWriter writer = new FileWriter("listMovies.csv");
+
+
+    //} 
+
+}
