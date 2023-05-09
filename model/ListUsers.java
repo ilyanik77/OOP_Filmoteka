33064f9b528d4ed75422df623c3253030b7eae79
ilya <ilyakhlamov77@gmail.com>
2023-05-09 19:19:05 +0300

    add ListUsers.java

---
+++ model/ListUsers.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ListUsers {
+
+    private ArrayList<User> listUsers;
+
+    public ArrayList<User> getListUsers(){
+        return listUsers;
+    }
+
+}
