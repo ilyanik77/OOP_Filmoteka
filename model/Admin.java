c654f31fe13d408d4c899370b495de7c6916eaac
ilya <ilyakhlamov77@gmail.com>
2023-05-13 15:31:51 +0300

    add Admin.java

---
+++ model/Admin.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Admin extends User {
+
+    public Admin(String name, String role, Double balance, int countFilms) {
+        super(name, role, balance, countFilms);
+        
+    }
+    
+}
